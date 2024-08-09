95e5a857e6632475619d202271bbd10c8192b07f
Dhu Eid <117572835+AbsoluteRich@users.noreply.github.com>
2024-08-09 14:06:54 +1000

    More scripting

--- Assets/Scripts/PlayerController2D.cs
+++ Assets/Scripts/PlayerController2D.cs
@@ -1,18 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController2D : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float movementSpeed = 2.0f;
+    Rigidbody2D _rb;
+    float _moveInput;
+    
+    void Awake()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        _moveInput = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 velocity = _rb.velocity;  // .velocity returns a temporary variable, so we can't directly set its value
+        velocity.x = _moveInput * movementSpeed;
+        _rb.velocity = velocity;
     }
 }
