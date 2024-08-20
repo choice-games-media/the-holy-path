c299d8f009273fea317c22912a700e606a02ef93
AdamMk2v <adamcarbonaro9@gmail.com>
2024-08-20 12:34:33 +1000

    continue working on movement

--- Assets/Scripts/PlayerController2D.cs
+++ Assets/Scripts/PlayerController2D.cs
@@ -3,9 +3,13 @@ using UnityEngine;
 public class PlayerController2D : MonoBehaviour
 {
     public float movementSpeed = 2.0f;
+    public float jumpSpeed = 4f;
+    public float groundRadiusCheck = 0.3f;
+    public LayerMask layers;
+
     Rigidbody2D _rb;
     float _moveInput;
-    
+    bool jumpInput = false;
     void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
@@ -14,12 +18,34 @@ public class PlayerController2D : MonoBehaviour
     void Update()
     {
         _moveInput = Input.GetAxis("Horizontal");
+        jumpInput = Input.GetButton("jump");
     }
 
     private void FixedUpdate()
     {
-        Vector2 velocity = _rb.velocity;  // .velocity returns a temporary variable, so we can't directly set its value
-        velocity.x = _moveInput * movementSpeed;
-        _rb.velocity = velocity;
+        Vector2 vel = _rb.velocity;  // .velocity returns a temporary variable, so we can't directly set its value
+        vel.x = _moveInput * movementSpeed;
+        bool onGround = GroundCheck();
+        if (jumpInput == true && onGround == true)
+        {
+            vel.y = jumpSpeed;
+        }
+
+        _rb.velocity = vel;
     }
+
+    bool GroundCheck()
+    {
+        Collider2D hitCollider = Physics2D.OverlapCircle(
+                             transform.position,
+                             groundRadiusCheck,
+                             layers);
+        return hitCollider != null;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, groundRadiusCheck);
+    }
+
 }
