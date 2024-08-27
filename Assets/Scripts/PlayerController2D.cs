183fb7f5b0857a0d18d01b903fa1652ff95036da
Dhu Eid <117572835+AbsoluteRich@users.noreply.github.com>
2024-08-27 12:29:27 +1000

    God I hate merge conflicts (part 1)
    Fixing errors + Readding some of the assets that got removed with the merge
    Player movement is mostly functional, with the exception of jumping

--- Assets/Scripts/PlayerController2D.cs
+++ Assets/Scripts/PlayerController2D.cs
@@ -6,27 +6,38 @@ public class PlayerController2D : MonoBehaviour
     public float jumpSpeed = 4f;
     public float groundRadiusCheck = 0.3f;
     public LayerMask layers;
-
     Rigidbody2D _rb;
     float _moveInput;
-    bool jumpInput = false;
+    bool _jumpInput;
+    private SpriteRenderer _characterSpriteRender;
+
     void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _characterSpriteRender = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
         _moveInput = Input.GetAxis("Horizontal");
-        jumpInput = Input.GetButton("jump");
+        _jumpInput = Input.GetButton("Jump");
+
+        if (_moveInput < 0)
+        {
+            _characterSpriteRender.flipX = false;
+        }
+        else if (_moveInput > 0)
+        {
+            _characterSpriteRender.flipX = true;
+        }
     }
 
     private void FixedUpdate()
     {
-        Vector2 vel = _rb.velocity;  // .velocity returns a temporary variable, so we can't directly set its value
+        Vector2 vel = _rb.velocity; // .velocity returns a temporary variable, so we can't directly set its value
         vel.x = _moveInput * movementSpeed;
         bool onGround = GroundCheck();
-        if (jumpInput == true && onGround == true)
+        if (_jumpInput && onGround)
         {
             vel.y = jumpSpeed;
         }
@@ -37,9 +48,9 @@ public class PlayerController2D : MonoBehaviour
     bool GroundCheck()
     {
         Collider2D hitCollider = Physics2D.OverlapCircle(
-                             transform.position,
-                             groundRadiusCheck,
-                             layers);
+            transform.position,
+            groundRadiusCheck,
+            layers);
         return hitCollider != null;
     }
 
@@ -47,5 +58,4 @@ public class PlayerController2D : MonoBehaviour
     {
         Gizmos.DrawWireSphere(transform.position, groundRadiusCheck);
     }
-
 }
