014bcffd0ab20e1dc8fdc235d4cc04871620f6ed
Dhu Eid <117572835+AbsoluteRich@users.noreply.github.com>
2024-10-07 09:40:20 +1100

    Damn lives counter

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,12 +1,14 @@
+using TMPro;
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
     public int health = 100;
+    public TextMeshProUGUI healthText;
     
     public void TakeDamage(int amount)
     {
         health -= amount;
-        print(health);
+        healthText.text = $"Lives: {health}";
     }
 }
