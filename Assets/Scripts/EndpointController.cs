da50267779343bf9d30501758b20422cfd9dc063
Dhu Eid <117572835+AbsoluteRich@users.noreply.github.com>
2024-10-14 00:03:22 +1100

    Uh oh

--- Assets/Scripts/EndpointController.cs
+++ Assets/Scripts/EndpointController.cs
@@ -1,16 +1,35 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class EndpointController : MonoBehaviour
 {
     public int newScene;
-    
+    public bool isEnd;
+    public TextMeshProUGUI victoryText = null;
+
+    private void Start()
+    {
+        victoryText.enabled = false;
+        gameObject.SetActive(true);
+    }
+
     private void OnTriggerEnter2D(Collider2D collidedObject)
     {
         if (collidedObject.CompareTag("Player"))
         {
             Destroy(gameObject);
+            
+            if (isEnd)
+            {
+                print("End!");
+                victoryText.enabled = true;
+                GameObject.FindGameObjectWithTag("Player").SetActive(false);
+                return;
+            }
+            
             SceneManager.LoadScene(newScene);
+
         }
     }
 }
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,16 +1,49 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
-    public int health = 100;
+    public int startingLives;
+    private int _lives;
     public TextMeshProUGUI healthText;
-    
+    public GameObject respawnPoint;
+
+    private void Start()
+    {
+        _lives = startingLives;
+        UpdateLifeCounter();
+    }
+
+    // https://medium.com/nerd-for-tech/implementing-a-life-system-unity-e2a9c6b44db5
     public void TakeDamage(int amount)
     {
-        print($"{health}: Taking {amount} damage");
-        health -= amount;
-        print(health);
-        healthText.text = $"Lives: {health}";
+        _lives -= amount;
+        UpdateLifeCounter();
+
+        if (_lives < 1)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            // gameObject.transform.localScale = new Vector3(0, 0, 0);
+            // StartCoroutine(EnablePlayer());
+        }
+    }
+    
+    /*
+    private IEnumerator EnablePlayer()
+    {
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        
+        // gameObject.transform.localScale = new Vector3(1, 1, 1);
+        // gameObject.transform.position = respawnPoint.transform.position;
+    }
+    */
+
+    private void UpdateLifeCounter()
+    {
+        print(_lives);
+        healthText.text = $"Lives: {_lives}";
     }
 }
